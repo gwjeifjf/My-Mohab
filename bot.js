6e5538aa58b7131101920ea43851f2eb84e2ce8f
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-11-21 18:58:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,14 +24,14 @@ client.on('ready', () => {
 
 
 
-const adminprefix = "#";
+const adminprefix = "";
 const devs = ['514114378384015372'];
 client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
 if (message.content.startsWith(adminprefix + 'st')) {
   client.user.setGame(argresult, "https://www.twitch.tv/idk")
-    message.channel.sendMessage(`**:white_check_mark: : ${argresult}**`)
+    message.channel.sendMessage(`**:white_check_mark: تم تغير الحاله الي  : ${argresult}**`)
 }
 });
 
