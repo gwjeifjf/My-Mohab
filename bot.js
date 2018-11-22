019762a53a84348378f7e2a19626711d561ae220
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-11-22 21:52:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,49 +20,6 @@ client.on('ready', () => {
 
 
 
-const developers = ["514114378384015372"]
-const adminprefix = "";
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'pl')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**Status You   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'wa')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**Status You   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'li')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**Status You  ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/gamer");
-      message.channel.send(`**Status You ${argresult} **`)
- 
-    } else 
-  }
-  if (message.content.startsWith(adminprefix + 'name')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'avatar')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
-}
-});
-
-client.on('guildMemberAdd', member => {
-const YossiF = member.guild.channels.get("503107906481160192");//ايدي الشات
-if(!YossiF) return;
-if(YossiF) {
-YossiF.send(`**__..Welcome :69:  To :69: Server Rocky..__**`);          
-}
-});
-
-
 
 
 
