a2987a703ae6d472edd5a508f316331915109c4b
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-11-22 11:37:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,6 +38,31 @@ if (message.content.startsWith(adminprefix + 'st')) {
 
 
 
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready',async () => {
+   let GUILDID = '469026947372941333'; //
+  let CHANNELID = '502994116112809999'; //
+
+
+
+  voiceStay(GUILDID, CHANNELID);
+  function voiceStay(guildid, channelid) {
+    if(!guildid) throw new Error('Syntax: voiceStay function requires guildid');
+    if(!channelid) throw new Error('Syntax: voiceStay function requires channelid');
+
+    let guild = client.guilds.get(guildid);
+    let channel = guild.channels.get(channelid);
+
+    if(channel.type === 'voice') {
+      channel.join().catch(e => {
+        console.log(`Failed To Join :: ${e.message}`);
+      });
+    } else {
+      console.log(`Channel Type :: ${channel.type}, It must be Voice.`);
+    }
+  }
+});
 
 
 
